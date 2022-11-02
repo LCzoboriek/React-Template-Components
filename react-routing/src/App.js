1a711a191adc01a0a3eb79c22723930137a92920
Luke Czoboriek <lc169782@protonmail.com>
2022-11-02 16:13:38 +0000

    Added in search field for mapping and filtering through a user list, and then effecting the particular routes that are shown

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -6,10 +6,13 @@ import {
   Outlet,
   NavLink,
   useParams,
+  useNavigate,
+  useSearchParams,
 } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  const navigate = useNavigate();
   const [users, setUsers] = React.useState([
     { id: "1", fullName: "Robin Wieruch" },
     { id: "2", fullName: "Sarah Finnley" },
@@ -17,6 +20,7 @@ const App = () => {
 
   const handleRemoveUser = (userId) => {
     setUsers((state) => state.filter((user) => user.id !== userId));
+    navigate("/users");
   };
 
   return (
@@ -47,23 +51,48 @@ const Home = () => {
 };
 //Users page/component
 const Users = ({ users }) => {
-  //Here is where we now accept the users prop from the Route component
+  //Here we are using the useSearchParams hook to get the search parameters from the url
+  const [searchParams, setSearchParams] = useSearchParams();
+  //We then set the searchTerm state to the search parameter, or an empty string if there is no search parameter
+  const searchTerm = searchParams.get("name") || "";
+  //We then create the handleSearch function, which will set the search parameter to the value of the search input
+  //via an event listener, if the search input is empty, it will remove the search parameter from the url and set it as an empty object
+  const handleSearch = (event) => {
+    const name = event.target.value;
+
+    if (name) {
+      setSearchParams({ name: event.target.value });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
-    <main>
-      <h2 className="Heading">Users</h2>
+    <>
+      <h2>Users</h2>
+
+      <input type="text" value={searchTerm} onChange={handleSearch} />
+
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={user.id}>{user.fullName}</Link>
-            {/*Here we are using the Link component to link to the user page, and we are passing the user id as a parameter*/}
-          </li>
-        ))}
+        {/* We then set the users list to be filtered, based on the search term if it exists, then we map through that array and output each one
+        with a link to each of those users and their id and first name, id personally change this to have use of a button so it limits the api
+        requests, but for this localised use theres no issue*/}
+        {users
+          .filter((user) =>
+            user.fullName.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          )
+          .map((user) => (
+            <li key={user.id}>
+              <Link to={user.id}>{user.fullName}</Link>
+            </li>
+          ))}
       </ul>
-      <Outlet />{" "}
-      {/*Here we are adding the outlet component, this is where the child component will be rendered, in this case the User component*/}
-    </main>
+
+      <Outlet />
+    </>
   );
 };
+
 //We now create the User component for the nested route, this will use the useParams hook to get the user id from the url and give us the relevant page
 const User = ({ onRemoveUser }) => {
   const { userId } = useParams(); //Here we are using the useParams hook to get the userId parameter from the url
