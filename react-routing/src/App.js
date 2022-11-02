d34edeb9372168206a4c98a55c9e31f146514132
Luke Czoboriek <lc169782@protonmail.com>
2022-11-02 13:12:41 +0000

    Tried adding in remove user functionality but users route broke, couldnt find any difference between the two code spaces so re copied in new source and that works

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,19 +1,38 @@
-import { Link, Routes, Route, Outlet, NavLink } from "react-router-dom";
+import * as React from "react";
+import {
+  Link,
+  Routes,
+  Route,
+  Outlet,
+  NavLink,
+  useParams,
+} from "react-router-dom";
 import "./App.css";
 
 const App = () => {
+  const [users, setUsers] = React.useState([
+    { id: "1", fullName: "Robin Wieruch" },
+    { id: "2", fullName: "Sarah Finnley" },
+  ]);
+
+  const handleRemoveUser = (userId) => {
+    setUsers((state) => state.filter((user) => user.id !== userId));
+  };
+
   return (
-    <>
-      <Routes>
-        {/* Layout wrapper */}
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="users" element={<Users />} />
-          <Route path="*" element={<NotFound />} />
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="users" element={<Users users={users} />}>
+          <Route
+            path=":userId"
+            element={<User onRemoveUser={handleRemoveUser} />}
+          />
         </Route>
-      </Routes>
-    </>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
 //Each of these components can be imagined as seperate pages, and the layout component is the wrapper for all of them. The layout component is the parent of
@@ -27,11 +46,36 @@ const Home = () => {
   );
 };
 //Users page/component
-const Users = () => {
+const Users = ({ users }) => {
+  //Here is where we now accept the users prop from the Route component
   return (
     <main>
       <h2 className="Heading">Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={user.id}>{user.fullName}</Link>
+            {/*Here we are using the Link component to link to the user page, and we are passing the user id as a parameter*/}
+          </li>
+        ))}
+      </ul>
+      <Outlet />{" "}
+      {/*Here we are adding the outlet component, this is where the child component will be rendered, in this case the User component*/}
     </main>
+  );
+};
+//We now create the User component for the nested route, this will use the useParams hook to get the user id from the url and give us the relevant page
+const User = ({ onRemoveUser }) => {
+  const { userId } = useParams(); //Here we are using the useParams hook to get the userId parameter from the url
+  return (
+    <>
+      <h2 className="Heading">User</h2>
+      <p>User ID: {userId}</p>
+      <button type="button" onClick={() => onRemoveUser(userId)}>
+        Remove User
+      </button>
+      <Link to="/users">Back to the users</Link>
+    </>
   );
 };
 //Layout component/template page, this is where you could put headers, footers and a navbar, and this will be rendered on all pages via the outlet component
